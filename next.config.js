37b2aa7f68b96ef9d695a997e77c1caa3ddc7252
Bai-ee <bryanballi@gmail.com>
2025-12-19 11:14:52 -0600

    Exclude critters_quest_miner-main folder from build and TypeScript compilation

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Exclude the critters_quest_miner-main folder from build
+  webpack: (config) => {
+    config.watchOptions = {
+      ...config.watchOptions,
+      ignored: ['**/critters_quest_miner-main/**'],
+    };
+    return config;
+  },
 };
 
 module.exports = nextConfig;
